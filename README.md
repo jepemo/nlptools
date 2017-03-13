0e8e008a11d2b542a989128aded7957476e20fa2
jepemo <Jeremias.Perez@desfici.com>
2017-03-13 20:01:25 +0100

    Basic tokenizer

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # nlptools
+*nlptools* are an Unix inspired utilities for Natural Language Processing.
+
+## Example
+
+```bash
+echo "Hello world!" | proc-tkn
+```
+
+Result:
+
+```
+Hello
+world
+!
+```
--- tools/tokenizer_nltk_py3.py
+++ tools/tokenizer_nltk_py3.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python
-
 # -*- coding: utf8 -*-
 
-import nltk
+import argparse
 import fileinput
+import nltk
 
-for line in fileinput.input():
-    tokens = nltk.word_tokenize(line.decode('utf-8'), language='spanish')
-    for t in tokens:
-        print(t.encode('utf-8').strip())
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Tokenize input and results one token per line')
+    parser.add_argument("--lang", type=str, default='english', help="Language (defalt: english): spanish, etc.")
+
+    args = parser.parse_args()
+
+    for line in fileinput.input():
+        # tokens = nltk.word_tokenize(line.decode('utf-8'), language=args.lang)
+        tokens = nltk.word_tokenize(line, language=args.lang)
+        for t in tokens:
+            # print(t.encode('utf-8').strip())
+            print(t.strip())
